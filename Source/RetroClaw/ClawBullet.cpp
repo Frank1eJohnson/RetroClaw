0eb8e7a3122b5a06ef832fdebf0c1ccf6d1805c7
Zhongqi Shan <60977360+Sabrave@users.noreply.github.com>
2019-05-12 06:59:09 +0800

    created bullet collision handlers

--- Source/RetroClaw/ClawBullet.cpp
+++ Source/RetroClaw/ClawBullet.cpp
@@ -2,13 +2,55 @@
 
 
 #include "ClawBullet.h"
+#include "PaperFlipbookComponent.h" 
+#include "Components/BoxComponent.h"
+#include "RetroClawCharacter.h"
+#include "EnemyCharacter.h"
+#include "Kismet/GameplayStatics.h"	
+#include "Components/CapsuleComponent.h" 
+#include "ClawGameMode.h"
+#include "Engine/Engine.h"
 
 AClawBullet::AClawBullet()
 {
 	PrimaryActorTick.bCanEverTick = false;
+
+	// initializes the Bullet's box collision 
+	HitCollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("Bullet Collision"));
+	HitCollisionBox->SetBoxExtent(FVector(5.0f, 5.0f, 5.0f));
+	HitCollisionBox->SetCollisionProfileName("Trigger");
+	HitCollisionBox->SetupAttachment(RootComponent);
+
+	HitCollisionBox->OnComponentBeginOverlap.AddDynamic(this, &AClawBullet::OnOverlapBegin);
+	HitCollisionBox->OnComponentEndOverlap.AddDynamic(this, &AClawBullet::OnOverlapEnd);
 }
 
 void AClawBullet::BeginPlay()
 {
 	Super::BeginPlay();
+
+	GameModeRef = Cast<AClawGameMode>(UGameplayStatics::GetGameMode(GetWorld()));
+
+	
+
+	UE_LOG(LogTemp, Warning, TEXT("started bullet"));
+}
+
+void AClawBullet::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	// check it it's claw who's overlapping with the score object.
+	if (OtherActor && OtherActor->IsA(AEnemyCharacter::StaticClass()) && OtherComp->IsA(UCapsuleComponent::StaticClass()))
+	{
+		// the bullet has hitted an enemy 
+
+		// decrease the enemy health.
+		//UGameplayStatics::ApplyDamage(OtherActor, Damage, GetOwner()->GetInstigatorController(), this, DamageType);
+
+		// destroy actor.
+		//this->Destroy();
+	}
+}
+
+void AClawBullet::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
 }
--- Source/RetroClaw/ClawBullet.h
+++ Source/RetroClaw/ClawBullet.h
@@ -6,9 +6,9 @@
 #include "PaperSpriteActor.h"
 #include "ClawBullet.generated.h"
 
-/**
- * 
- */
+
+class AClawGameMode;
+
 UCLASS()
 class RETROCLAW_API AClawBullet : public APaperSpriteActor
 {
@@ -22,8 +22,18 @@ private:
 	TSubclassOf<UDamageType> DamageType;
 
 	UPROPERTY(EditAnywhere, Category = Damage, meta = (AllowPrivateAccess = "true"))
-	float Damage = 101;
+	float Damage = 100;
 
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	class UBoxComponent* HitCollisionBox;
+
+	AClawGameMode* GameModeRef;
 
 public:
 	// class constructor
